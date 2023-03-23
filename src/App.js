3e96369e8c4474d10bbbf7384703e3ff3c0a72de
saikumar90143 <saikumar.dudala@gmail.com>
2023-03-23 22:51:15 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
+import React, { useEffect, useState} from "react";
 
-import React, { useEffect, useState ,Suspense} from 'react'
-
-import './App.css'
-import Aboutme from './components/Aboutme'
-import ContactMe from './components/ContactMe'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Loader from './components/Loader'
-import Skills from './components/Skills'
-import WhatIDo from './components/WhatIDo'
-const Projects = React.lazy(()=>import ('./components/Projects' ))  
+import "./App.css";
+import Aboutme from "./components/Aboutme";
+import ContactMe from "./components/ContactMe";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Loader from "./components/Loader";
+import Skills from "./components/Skills";
+import WhatIDo from "./components/WhatIDo";
+import Projects from "./components/Projects";
 function App() {
-  window.onload= function () {
-    window.scrollTo(0,0);
-};
-  const [loading,setLoading]=useState(false)
-    useEffect(()=>{
-setTimeout(()=>{
-   setLoading(true)
-},2500)
-return()=>{
-  setLoading(false)
-}
-    },[window.onloadstart])
+  window.onload = function () {
+    window.scrollTo(0, 0);
+  };
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true);
+    }, 2500);
+    return () => {
+      setLoading(false);
+    };
+  }, [window.onloadstart]);
 
   return (
     <div className="App">
-      {!loading?(<Loader/>):(
+      {!loading ? (
+        <Loader />
+      ) : (
         <>
-        <Header/>
-        
-     <Hero/>
-     <Aboutme/>
-     <WhatIDo/>
-     <Skills/>
-     <Suspense fallback={<Loader/>}>
-
-     <Projects/>
-     </Suspense>
-     <ContactMe/>
+          <Header />
+          <Hero />
+          <Aboutme />
+          <WhatIDo />
+          <Skills />
+          <Projects />
+          <ContactMe />
         </>
       )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter } from 'react-router-dom';
+import Loader from './components/Loader';
+const App=React.lazy(()=>import ('./App'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <Suspense fallback={<Loader/>}>   
     <App />
+     </Suspense>
     </BrowserRouter>
   </React.StrictMode>,
 )
