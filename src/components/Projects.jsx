cbb460394a20a5e0e229cc313570acc0d29a024d
saikumar90143 <saikumar.dudala@gmail.com>
2023-09-14 19:13:16 +0530

    latest commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,13 +12,14 @@ import {
 } from "mdb-react-ui-kit";
 import React from "react";
 import styled from "styled-components";
-import ecommerce from "../assets/ecom.png";
+import nightclub from "../assets/night-club.png";
 import fitness from "../assets/gym.png";
 import tourapp from "../assets/tour.png";
 import foodapp from "../assets/food.png";
 import react from "../assets/react.png";
 import redux from "../assets/redux.png";
 import html from "../assets/html5.png";
+import javascript from "../assets/javascript.png";
 import sass from "../assets/sass-avatar.png";
 import api from "../assets/api.png";
 import nodejs from "../assets/nodejs.png";
@@ -31,6 +32,7 @@ const Projects = () => {
       title: "Foodies App",
       desc: "A UI for foodies app created using ReactJS and Redux-toolkit",
       sourcecode:"https://github.com/saikumar90143/restaruant-app-client",
+      seelive:"https://64fff6dda6f3c420340765d8--melodic-alfajores-204df1.netlify.app/",
       techStack: [react, redux, sass, html, api],
     },
     {
@@ -51,11 +53,12 @@ const Projects = () => {
       techStack: [react, sass, html],
     },
     {
-      image: ecommerce,
-      title: "Ecommerce Website",
-      desc: "This is an ecommerce website designed using react and fetch products through api and manage products to add to cart using redux",
-     seelive:"https://busystore-ecom-42f9a.web.app/",
-      techStack: [react, sass, html, redux, api],
+      image: nightclub,
+      title: "Night Club",
+      desc: "This is an user-friendly website designed using react",
+     seelive:"https://night-club.onrender.com/",
+     sourcecode:"https://github.com/saikumar90143/night-club",
+      techStack: [react, sass, javascript],
     },
   ];
   return (
@@ -86,14 +89,17 @@ const Projects = () => {
                         : project.desc}
                     </p>
                     <div className="d-flex justify-content-between flex-wrap">
+                        <a href={project?.seelive}>
                       <MDBBtn>
+                          
                         See Live{" "}
                         <MDBIcon
                           fas
                           icon="long-arrow-alt-right"
                           className="arrow"
-                        />
+                          />
                       </MDBBtn>
+                          </a>
                       <a href={project.sourcecode}>
                       
                       <MDBBtn>
